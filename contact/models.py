407036c3a6894c24be82aab51a85cee3219dffcb
Gabriel ARP <garpereira@unifesp.br>
2025-04-19 18:20:12 -0300

    :sparkles: feat: Cria model Contact e faz migrações da alteração

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=12)
+    email = models.EmailField(max_length=255, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+
+

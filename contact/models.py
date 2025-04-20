6b8ca37f87b8b2858a19e2f85a7383c7a12f8204
Gabriel ARP <garpereira@unifesp.br>
2025-04-19 22:50:17 -0300

    :sparkles: feat: Cria Models Category e adiciona chave estrangeira em Contact

--- contact/models.py
+++ contact/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    None
+    name = models.CharField(max_length=50, default='')
+
+    def __str__(self):
+        return f"{self.name}"
 
 # Create your models here.
 class Contact(models.Model):
@@ -14,8 +17,8 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    cateogry = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
     

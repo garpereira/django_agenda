2cb4b20c1fe8e27896cbf75159ff6ad58d38c28c
Gabriel ARP <garpereira@unifesp.br>
2025-04-19 22:35:30 -0300

    :sparkles: feat: Configura patterns MEDIA e STATIC
    Atualiza models com show e image_field\nConfigura Admin Panel\nAdiciona Migrations

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
+class Category(models.Model):
+    None
+
 # Create your models here.
 class Contact(models.Model):
     first_name = models.CharField(max_length=15)
@@ -8,5 +12,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=255, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
-
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    

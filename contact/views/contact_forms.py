e501adef2ae73404985371e891c5a92f6c227e4d
Gabriel ARP <garpereira@unifesp.br>
2025-04-21 10:55:43 -0300

    :sparkles: feat: Cria a rota de atualização de informacoes de contato

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
+from django.urls import reverse
+
+from contact.models import Contact
 
 def create(request):
+    form_action = reverse('contact:create')
+
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
         context = {
             'form': form,
+            'form_action': form_action,
+
         }
         
         if form.is_valid():
             # contact = form.save(commit=False) # Segura o formulario aqui para fazer alterações antes de salvar
             contact = form.save()
-            
-            return redirect('contact:create')
+            return redirect('contact:update', contact_id=contact.pk)
 
         return render(
             request,
@@ -22,7 +29,45 @@ def create(request):
 
         
     context = {
-        'form': ContactForm()
+        'form': ContactForm(),
+        'form_action': form_action,
+    }
+    return render(
+        request,
+        "contact/create.html",
+        context,
+    )
+
+
+def update(request, contact_id):
+    contact = get_object_or_404(
+            Contact, pk=contact_id, show=True
+        )
+    form_action = reverse('contact:update', args=(contact_id,))
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+        
+        if form.is_valid():
+            # contact = form.save(commit=False) # Segura o formulario aqui para fazer alterações antes de salvar
+            print("É valido sisi")
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+
+        return render(
+            request,
+            "contact/create.html",
+            context,
+        )
+
+        
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action,
     }
     return render(
         request,

7b4df6cde48a8f61673ca54674cbce61f44408ed
Gabriel ARP <garpereira@unifesp.br>
2025-04-21 09:27:02 -0300

    :sparkles: feat: Cria validação do formulário e salva no DB. Cria link para criar formulario

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         context = {
-            'form': ContactForm(request.POST)
+            'form': form,
         }
+        
+        if form.is_valid():
+            # contact = form.save(commit=False) # Segura o formulario aqui para fazer alterações antes de salvar
+            contact = form.save()
+            
+            return redirect('contact:create')
+
         return render(
             request,
             "contact/create.html",
             context,
         )
+
         
     context = {
         'form': ContactForm()

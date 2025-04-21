f3bc80639559cdb250f10e88beb7834ddd1a64e3
Gabriel ARP <garpereira@unifesp.br>
2025-04-21 08:40:44 -0300

    :books: docs: aprendendo sobre validacao dos campos do formulario

--- contact/forms.py
+++ contact/forms.py
@@ -4,28 +4,28 @@ from django import forms
 
 class ContactForm(forms.ModelForm):
 
-    first_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs= {
-                'class': 'classe-a classe-b', # atributos do widgets
-                'placeholder': "Escreva aqui",
-            }
-        ),
-        label="Primeiro Nome",
-        help_text="Texto de ajuda para o usuario",
-    )
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs= {
+    #             'class': 'classe-a classe-b', # atributos do widgets
+    #             'placeholder': "Escreva aqui",
+    #         }
+    #     ),
+    #     label="Primeiro Nome",
+    #     help_text="Texto de ajuda para o usuario",
+    # )
 
     # Este campo nao esta ligado ao model, poderia ser um re captcha?
-    campo_qualquer = forms.CharField(
-        widget=forms.TextInput(
-            attrs= {
-                'class': 'classe-a classe-b', # atributos do widgets
-                'placeholder': "Escreva aqui",
-            }
-        ),
-        label="Qualquer",
-        help_text="Texto de ajuda para o usuario",
-    )
+    # campo_qualquer = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs= {
+    #             'class': 'classe-a classe-b', # atributos do widgets
+    #             'placeholder': "Escreva aqui",
+    #         }
+    #     ),
+    #     label="Qualquer",
+    #     help_text="Texto de ajuda para o usuario",
+    # )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -53,13 +53,39 @@ class ContactForm(forms.ModelForm):
     
     def clean(self):
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
+        msg = ValidationError(
+            'Primeiro nome não pode ser igual ao segundo',
+            code='invalid'
         )
-        
+        if first_name == last_name:
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Mensagem de erro do clean',
+        #         code='invalid'
+        #     )
+        # )
         return super().clean()
+
+    # Da para tratar o que é recebido no post do forms antes de fato ir para o banco
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            # raise ValidationError( # raise faz o codigo parar neste ponto
+            #     'Não digite ABC neste campo',
+            #     code='invalid',
+            # )
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Mensagem de erro do clean_first_name',
+                    code='invalid',
+                )
+            )  
+        return first_name

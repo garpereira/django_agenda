01387302bd49c97cc49fdba22faafe1f1863f4b8
Gabriel ARP <garpereira@unifesp.br>
2025-04-22 12:41:48 -0300

    :sparkles: feat: Adiciona mensagens de sucesso no registro e deixa o formulário em parte

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from contact.forms import RegisterForm
 
 def register(request):
     user = RegisterForm()
+    # messages.info(request, 'um texto qualquer')
+    # messages.success(request, 'um texto qualquer')
+    # messages.warning(request, 'um texto qualquer')
+    # messages.error(request, 'um texto qualquer')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado com sucesso!')
+            return redirect('contact:register')
 
         return render(
         request,
